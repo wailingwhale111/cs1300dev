6413ffa48c492095c9596881705dd2e31a8ea948
christinej604 <77814489+christinej604@users.noreply.github.com>
2022-11-21 21:23:01 -0500

    pictures show up

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
+import nctData from "./assets/nct-data.json";
+import NCTItem from "./components/NCTItem";
+
+nctData.forEach((item) => {
+  item.image = process.env.PUBLIC_URL + "/" + item.image;
+});
 
 function App() {
+  // TODO: use useState to create a state variable to hold the state of the cart
+  /* add your cart state code here */
+  const [cart, setCart] = useState([]);
+  let total = 0.00;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My NCT Universe</h1> 
+      {nctData.map((item, index) => (
+       <NCTItem 
+       index={index}
+       name={item.name} 
+       image={item.image} 
+       position={item.position} 
+       group={item.group}
+       alphabetical={item.alphabetical}
+       setCart={setCart}
+       cart={cart}
+
+       ></NCTItem>
+      ))}
+
+      {/* <div>
+        <h2>Cart</h2>
+        {cart.map(({name, price, quantity}) => {
+          total += price * quantity
+          return (<p>{quantity} x {name}: ${price} </p>);
+        })}
+        <p>Total: $ {total}</p>
+        
+      </div> */}
     </div>
   );
 }
---
+++ my-app/src/components/NCTMember.js
@@ -0,0 +1,29 @@
+import "./NCTItem.css"
+export default function NCTItem(props) {
+	// function handleClick() {
+  //   const itemInCart = {name: props.name, price: props.price, quantity: 1};
+  //   const itemExistInCart = props.cart.find((element) => element.name === props.name);
+  //   if (itemExistInCart !== undefined) {
+  //     props.setCart(
+  //       props.cart.map((element) => {
+  //         if (element === itemExistInCart) {
+  //           return {name: props.name, price: props.price, quantity: element.quantity + 1};
+  //         } else {
+  //           return element;
+  //         }
+  //       })
+  //     );
+  //   } else {
+  //     props.setCart(props.cart.concat(itemInCart));
+  //   }
+  // };
+  return (
+		 <div>
+        <h2> {props.name}</h2>
+        <img src={props.image} alt={props.group}/>
+        <p> Group: {props.group}</p>
+        <p> Position: {props.position}</p>
+        <button>Add to Cart</button>
+      </div>
+	);
+}
