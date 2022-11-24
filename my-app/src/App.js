c3ce31346e5c67cf3bfb7d00bf53c6cb866fbec0
christinej604 <77814489+christinej604@users.noreply.github.com>
2022-11-24 01:21:40 -0500

    finished adding and removing

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import './App.css';
 import nctData from "./assets/nct-data.json";
 import NCTMember from "./components/NCTMember";
-import { Container, Row, Col, Card, Form} from "react-bootstrap";
+import { Container, Row, Col, Form, ListGroup, Card} from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -13,29 +13,34 @@ nctData.forEach((member) => {
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
-  const [cart, setCart] = useState([]);
-  let total = 0.00;
+  const [biasList, setBiasList] = useState([]);
+  const [totalNum, setTotalNum] = useState(0);
+  let total = 0;
 
+  let filteredData = nctData.filter(member => member.group.includes("NCT 2020"));
+  
   return (
     <div className="App">
       <Container>
         <Row>
           <Col>
-            <h1>NCT Universe</h1> 
+            <h1><strong>My NCT Universe</strong></h1> 
             <Container>
               <Row>
-                {nctData.map((member, index) => (
+                {filteredData.map((member, index) => (
                  <Col id={index} className="d-flex justify-content-around align-items-stretch">
                    <NCTMember 
-                index={index} 
-                name={member.name} 
-                image={member.image} 
-                position={member.position} 
-                group={member.group} 
-                alphabetical={member.alphabetical} 
-                age={member.age}
-                setCart={setCart} 
-                cart={cart}/>
+                    index={index} 
+                    name={member.name} 
+                    image={member.image} 
+                    position={member.position} 
+                    group={member.group} 
+                    alphabetical={member.alphabetical} 
+                    age={member.age}
+                    setBiasList={setBiasList} 
+                    biasList={biasList}
+                    totalNum={totalNum}
+                    setTotalNum={setTotalNum}/>
                 </Col>))}
               </Row>
             </Container>
@@ -43,90 +48,103 @@ function App() {
           <Col lg={3}>
             <Card>
               <Card.Body>
-                <Card.Text>
-                  <h4>Sort by:</h4>
-                  <Form>
-                    <div key={`radio`} className="mb-3">
-                      <Form.Check
-                        type={`radio`}
-                        id={`radio`}
-                        label={'Age'}
-                        defaultChecked
-                      />
-                      <Form.Check
-                        type={`radio`}
-                        id={`radio1`}
-                        label={`Alphabetical`}
-                      />
-                    </div>
-                  </Form>
-                  <h4>Filter by:</h4>
-                  <strong>Position</strong>
-                  <Form>
-                    <div key={`default-checkbox`} className="mb-3">
-                      <Form.Check 
-                        type={`checkbox`}
-                        id={`default-checkbox1`}
-                        label={`Vocalist`}
-                      />
-                      <Form.Check
-                        type={`checkbox`}
-                        id={`default-checkbox2`}
-                        label={`Rapper`}
-                      />
-                      <Form.Check
-                        type={`checkbox`}
-                        id={`default-checkbox3`}
-                        label={`Dancer`}
-                      />
-                      <Form.Check
-                        type={`checkbox`}
-                        id={`default-checkbox4`}
-                        label={`Leader`}
-                      />
-                    </div>
-                  </Form>
-                  <strong>Sub-Groups</strong>
-                  <Form>
-                    <div key={`default-checkbox`} className="mb-3">
-                      <Form.Check 
-                        type={`checkbox`}
-                        id={`default-checkbox1`}
-                        label={`NCT 127`}
-                      />
-                      <Form.Check
-                        type={`checkbox`}
-                        id={`default-checkbox2`}
-                        label={`NCT DREAM`}
-                      />
-                      <Form.Check
-                        type={`checkbox`}
-                        id={`default-checkbox3`}
-                        label={`NCT U`}
-                      />
-                      <Form.Check
-                        type={`checkbox`}
-                        id={`default-checkbox4`}
-                        label={`NCT 2018`}
-                      />
-                      <Form.Check
-                        type={`checkbox`}
-                        id={`default-checkbox4`}
-                        label={`NCT 2020`}
-                      />
-                      <Form.Check
-                        type={`checkbox`}
-                        id={`default-checkbox4`}
-                        label={`NCT 2021`}
-                      />
-                    </div>
-                  </Form>
-                </Card.Text>
-              </Card.Body>
-              <Card.Footer>
+              
+              <strong>Sort by: </strong>
+                <Form>
+                  <div key={`radio`} className="mb-3">
+                    <Form.Check
+                      name="sort"
+                      type={`radio`}
+                      id={`radio`}
+                      label={'Age'}
+                      defaultChecked
+                    />
+                    <Form.Check
+                      name="sort"
+                      type={`radio`}
+                      id={`radio1`}
+                      label={`Alphabetical`}
+                    />
+                  </div>
+                </Form>
+                <strong>Filter by:</strong>
+                <strong>Position</strong>
+                <Form>
+                  <div key={`default-checkbox`} className="mb-3">
+                    <Form.Check 
+                      type={`checkbox`}
+                      id={`default-checkbox1`}
+                      label={`Vocalist`}
+                    />
+                    <Form.Check
+                      type={`checkbox`}
+                      id={`default-checkbox2`}
+                      label={`Rapper`}
+                    />
+                    <Form.Check
+                      type={`checkbox`}
+                      id={`default-checkbox3`}
+                      label={`Dancer`}
+                    />
+                    <Form.Check
+                      type={`checkbox`}
+                      id={`default-checkbox4`}
+                      label={`Leader`}
+                    />
+                  </div>
+                </Form>
+                <strong>Sub-Groups</strong>
+                <Form>
+                  <div key={`default-checkbox`} className="mb-3">
+                    <Form.Check 
+                      type={`checkbox`}
+                      id={`default-checkbox1`}
+                      label={`NCT 127`}
+                      // onChange={selectFilterType}
+                    />
+                    <Form.Check
+                      type={`checkbox`}
+                      id={`default-checkbox2`}
+                      label={`NCT DREAM`}
+                      // onChange={filteredData = filteredData.filter(member => member.group.includes("NCT DREAM"))}
+                    />
+                    <Form.Check
+                      type={`checkbox`}
+                      id={`default-checkbox3`}
+                      label={`NCT U`}
+                      // onChange={selectFilterType}
+                    />
+                    <Form.Check
+                      type={`checkbox`}
+                      id={`default-checkbox4`}
+                      label={`NCT 2018`}
+                      // onChange={selectFilterType}
+                    />
+                    <Form.Check
+                      type={`checkbox`}
+                      id={`default-checkbox4`}
+                      label={`NCT 2020`}
+                      // onChange={selectFilterType}
+                    />
+                    <Form.Check
+                      type={`checkbox`}
+                      id={`default-checkbox4`}
+                      label={`NCT 2021`}
+                      // onChange={selectFilterType}
+                    />
+                  </div>
+                </Form>        
                 <strong>My Bias List:</strong>
-              </Card.Footer>
-            </Card>
+                {biasList.map(({name, age}) => {
+                  total += age
+                    return (
+                    <ListGroup.Item>
+                      {name}: {age}
+                    </ListGroup.Item>
+                  )})}
+                </Card.Body>
+                <Card.Footer>Average Age of My Bias: {totalNum === 0 ? "" : total / totalNum}</Card.Footer>
+              </Card>
           </Col>
         </Row>
       </Container>
